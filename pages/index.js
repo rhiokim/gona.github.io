5471cb574294deee7632b7ac7740d32f5d7e47f3
rhiokim <rhio.kim@gmail.com>
2017-05-22 02:44:53 +0900

    feat: translation and improve style

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,40 @@ import Layout from '../components/layout'
 export default () => (
   <Layout>
     <div className="container">
-      <div className="section philosophy center">
+      <div className="section philosophy">
         <div className="container">
-          <h2 className="title">Do you <b>gonna</b> <u>do</u> something?</h2>
+          <h2 className="title"><strong>Gona</strong></h2>
           <div className="row story">
-            Tasks, Meetings, Alarm and Articles... <br/>
+            Gona is Electron based Application to manage tasks, meetings and alarm.<br/>
+            And it's inspired by <i>I'm <strong>gonna</strong> <u>do</u> something</i><br/>
+            <br/>
+            <code>$ gona do 'task'</code>
           </div>
         </div>
       </div>
 
-      <div className="section philosophy center">
+      <div className="section philosophy">
+        <div className="container">
+          <h2 className="title">Do you <b>gonna</b> <u>do</u> something?</h2>
+          <div className="row story">
+            할일이 많다는 건<br/>
+            그것은 오늘 할일을 집중하지 못하게 해요.<br/>
+            <br/>
+            <strong><u>Only five important things</u></strong><br/>
+            It is enough today with <strong>five important things</strong> to do.<br/>
+            You already have done a lot of things today<br/>
+            <br/>
+            Please don't be rush<br/>
+            <br/>
+            Walk slowly...<br/>
+            And allow the world to see your step.<br/>
+            <br/>
+            #YOLO
+          </div>
+        </div>
+      </div>
+
+      <div className="section concept">
         <div className="container">
           <h4 className="section-heading"><b>Gona</b>: CLI and GUI</h4>
           <img src="/static/images/gona-cli.png" />
@@ -21,25 +45,19 @@ export default () => (
         </div>
       </div>
 
-      <div className="section philosophy center">
-        <div className="container">
-          <h4 className="section-heading">Need help getting started?</h4>
-          <p className="section-description">할일을 너무 복잡하게 만들지 마세요.  <br/>
-            그것은 오늘 할일을 집중하지 못하게 해요.<br/>
-            <br/>
-            딱 5개만 등록하고 그일을 마치고 나면 그때 다시 생각하세요.<br/>
-            당신이 오늘 5개의 할일을 마치는 것 만으로 충분한 하루를 보낸거에요.<br/>
-            <br/>
-            조금 느리게 걸어요.<br/>
-            <br/>
-            세상이 당신에게 발을 맞출 수 있게...</p>
-        </div>
-      </div>
       <div className="section get-help">
         <div className="container">
           <h4 className="section-heading">Need help getting started?</h4>
           <p className="section-description">Gona is dead simple to manage TODO. If you want to learn more, just visit the help!</p>
-          <a className="button button-primary" href="http://getskeleton.com">View Tutorial Videos</a>
+        </div>
+      </div>
+
+      <div className="section download">
+        <div className="container">
+          <h4 className="section-heading">Download</h4>
+          <a className="button button-primary" href="https://github.com/rhiokim/gona/releases">Mac OS</a>
+          <a className="button button-primary" href="https://github.com/rhiokim/gona/releases">Win</a>
+          <a className="button button-primary" href="https://github.com/rhiokim/gona/releases">Linux</a>
         </div>
       </div>
     </div>
